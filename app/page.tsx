2435828763e8854ae69263069229741a0221e9ed
owenmicheal <owenmicheal3rd@gmail.com>
2026-02-13 11:27:28 +0300

    feat: call events from the databse in the ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,42 @@
+import EventCard from "@/components/EventCard";
+import { IEvent } from "@/database";
+import { cacheLife } from "next/cache";
 import ExploreButton from "@/components/ExploreButton";
 
-const Home = () => {
-    return (
-        <section className="mt-15">
-            <h1 className="text-center">
-                The hub for Every Dev <br/> Event You Cant Miss
-            </h1>
-            <p className="text-center mt-5">Hackathons, Meet ups and Conferences, All in One Place</p>
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-            <ExploreButton />
-        </section>
-    )
-}
-export default Home;
+const Page = async () => {
+  "use cache";
+  cacheLife("hours");
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const { events } = await response.json();
 
+  return (
+    <section className="mt-15">
+      <h1 className="text-center">
+        The Hub for Every Dev <br /> Event You Can't Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences, All in One Place
+      </p>
+
+      <ExploreButton />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+
+        <ul className="events">
+          {events &&
+            events.length > 0 &&
+            events.map((event: IEvent) => (
+              <li key={event.title} className="list-none">
+                <EventCard {...event} />
+              </li>
+            ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Page;
