2d6a66c578da1f76f494bdc214eb400a653d3939
owenmicheal <owenmicheal3rd@gmail.com>
2026-02-13 16:20:46 +0300

    feat: event details page

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ const Page = async () => {
   return (
     <section className="mt-15">
       <h1 className="text-center">
-        The Hub for Every Dev <br /> Event You Can't Miss
+        The Hub for Every Dev <br /> Event You Can`t Miss
       </h1>
       <p className="text-center mt-5">
         Hackathons, Meetups, and Conferences, All in One Place
